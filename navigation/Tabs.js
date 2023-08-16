6123a87798740b4bf3fdff6a075f678d291465a4
hwacho9 <63436365+hwacho9@users.noreply.github.com>
2023-08-16 21:26:04 +0900

    feat:Configuring the Navigation

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,15 +1,40 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Movies from '../screens/Movies';
-import Tv from '../screens/Tv';
-import Search from '../screens/Search';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Movies from "../screens/Movies";
+import Tv from "../screens/Tv";
+import Search from "../screens/Search";
+import { View, Text } from "react-native";
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => <Tab.Navigator>
-    <Tab.Screen name="Movies" component={Movies} />
-    <Tab.Screen name="Tv" component={Tv} />
-    <Tab.Screen name="Search" component={Search} />
-</Tab.Navigator>
+//screenOptions는 navigator의 모든 screen 에 적용
+const Tabs = () => (
+    <Tab.Navigator
+        initialRouteName="Movies"
+        screenOptions={{
+            tabBarActiveTintColor: "red",
+        }}
+    >
+        <Tab.Screen
+            name="Movies"
+            component={Movies}
+            options={{
+                headerRight: () => (
+                    <View>
+                        <Text>Hello</Text>
+                    </View>
+                ),
+            }}
+        />
+        <Tab.Screen
+            name="Tv"
+            component={Tv}
+            options={{
+                tabBarBadge: "hi",
+            }}
+        />
+        <Tab.Screen name="Search" component={Search} />
+    </Tab.Navigator>
+);
 
-export default Tabs; 
+export default Tabs;
